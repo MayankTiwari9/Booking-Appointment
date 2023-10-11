d93258a579cd1a673b5ffc4adb9e09af7f63f90f
MayankTiwari9 <mt1027319@gmail.com>
2023-10-11 18:26:42 +0530

    Delete user from UI and LocalStorage

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ form.addEventListener('submit', function(e){
         email: email,
         number: number
     };
+
     saveFormData(formData);
     location.reload();
 });
 
+//Saving in local storage
 const storedData = JSON.parse(localStorage.getItem('formData')) || [];
 function saveFormData(formData){
 
@@ -32,6 +34,7 @@ function displayStoredData() {
         let listitem = document.createElement('li');
         listitem.textContent = element.name + '-' + element.email + '-' + element.number;
         
+        // Delete Functionality
         let delBtn = document.createElement('button');
         delBtn.textContent = 'Delete';
         delBtn.type = 'button';
@@ -42,7 +45,28 @@ function displayStoredData() {
             displayStoredData();
         });
 
+        // Edit functionality
+        let editBtn = document.createElement('button');
+        editBtn.textContent = 'Edit';
+        editBtn.type = 'button';
+        editBtn.addEventListener('click', function(){
+
+            let nameInput = document.getElementById('name');
+            nameInput.value = element.name;
+            let emailInput = document.getElementById('email');
+            emailInput.value = element.email;
+            let numberInput = document.getElementById('number');
+            numberInput.value = element.number;
+
+            storedData.splice(index, 1);
+            localStorage.setItem('formData', JSON.stringify(storedData));
+
+            displayStoredData();
+        })
+
+        // Appending delete and edit to li and li to ul
         listitem.appendChild(delBtn);
+        listitem.appendChild(editBtn);
         list.appendChild(listitem);
     });
 }
